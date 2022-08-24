336ca2cb9c9bd0657627874875adc3b5f2d064a7
shahbaz <sk43479@gmail.com>
2022-08-24 16:14:41 +0530

    back

--- lib/animations.dart
+++ lib/animations.dart
@@ -40,7 +40,7 @@ class _AnimationsState extends State<Animations> {
                     });
 
                     // reset the TextField
-
+                    //new chnages made....
                     // Close the dialog
                     Navigator.of(context).pop();
                   },
